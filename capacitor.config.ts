685031b5480be70071fa9c2a96bebaa45f189319
Agustin Navarro <xAgustin93@gmail.com>
2022-07-12 22:58:25 +0200

    Change app name and icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
-import { CapacitorConfig } from '@capacitor/cli';
+import { CapacitorConfig } from "@capacitor/cli";
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'todo',
-  webDir: 'build',
-  bundledWebRuntime: false
+  appId: "io.ionic.starter",
+  appName: "Todo List",
+  webDir: "build",
+  bundledWebRuntime: false,
 };
 
 export default config;
